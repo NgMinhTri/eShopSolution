7fc9668d8b2be846733768a7273e26ea7313faa3
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-10-19 10:59:49 +0700

    Liệt kê danh sách sản phẩm trong giỏ hàng

--- eShopSolution.WebApp/Controllers/CartController.cs
+++ eShopSolution.WebApp/Controllers/CartController.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace eShopSolution.WebApp.Controllers.Components
+namespace eShopSolution.WebApp.Controllers
 {
     public class CartController : Controller
     {
@@ -16,6 +16,16 @@ namespace eShopSolution.WebApp.Controllers.Components
         public CartController(IProductApiClient productApiClient)
         {
             _productApiClient = productApiClient;
+        }
+
+        [HttpGet]
+        public IActionResult GetListItems()
+        {
+            var session = HttpContext.Session.GetString(SystemConstants.CartSession);
+            List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
+            if (session != null)
+                currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
+            return Ok(currentCart);
         }
         public IActionResult Index()
         {
@@ -41,11 +51,12 @@ namespace eShopSolution.WebApp.Controllers.Components
                 Description = product.Description,
                 Image = product.ThumbnailImage,
                 Name = product.Name,
+                Price = product.Price,
                 Quantity = quantity
             };
             currentCart.Add(cartItem);
             HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
-            return Ok();
+            return Ok(currentCart);
         }
     }
 }
